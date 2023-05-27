59d63a27041285c79c6b32857a336aee8ea1fd8f
genie4days <godsonoronsaye@gmail.com>
2023-05-27 16:13:13 +0100

    Updated Terraform Files

--- main.tf
+++ main.tf
@@ -1,273 +1,110 @@
 provider "aws" {
-  region = "eu-west-2"
+    region = var.region
 }
 
 data "aws_ssm_parameter" "instance_ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-#Creating VPC
-resource "aws_vpc" "Terra-vpc" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
-  enable_dns_hostnames = "true"
-  enable_dns_support = "true"
+resource "aws_security_group" "cba_tf_sg" {
+ name        = "cba_tf_sg"
+ description = "allow all traffic"
+ vpc_id = aws_vpc.my_vpc.id
+ ingress {
+   from_port  = 22
+   to_port    = 22
+   protocol   = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+ ingress {
+   from_port  = 80
+   to_port    = 80
+   protocol   = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+ egress {
+   from_port  = 0
+   to_port    = 0
+   protocol   = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+ tags = {
+   name = "CBAterraformSG"
+ }
+}
+
+resource "aws_instance" "cba_tf_instance" {
+  ami             = data.aws_ssm_parameter.instance_ami.value
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.cba_public.id
+  security_groups = [aws_security_group.cba_tf_sg.id]
+  key_name        = var.key_name
+  user_data       = fileexists("install_apache.sh") ? file("install_apache.sh") : null
+
 
   tags = {
-    Name = "Terra-vpc"
+    "NAME" = "ApacheInstance"
+  }
+
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  instance_tenancy     = "default"
+  tags = {
+    name = "ApacheVPC"
   }
 }
 
-#Creating Internet Gateway
-resource "aws_internet_gateway" "Terraform-IGW" {
-  vpc_id = aws_vpc.Terra-vpc.id
-}
-
-#Creating 1st Public Subnet
-resource "aws_subnet" "terraform_public_subnet01" {
-  vpc_id                  = aws_vpc.Terra-vpc.id
-  cidr_block              = var.subnet1_cidr
-  map_public_ip_on_launch = true
+resource "aws_subnet" "cba_public" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
   availability_zone       = "eu-west-2a"
 
   tags = {
-    Name = "Terraform-public-subnet01"
+    Name = "ApachePublicSubnet"
   }
 }
 
-#Creating public subnet in region 2
-resource "aws_subnet" "terraform_public_subnet02" {
-  vpc_id                  = aws_vpc.Terra-vpc.id
-  cidr_block              = var.subnet2_cidr
-  map_public_ip_on_launch = true
+resource "aws_subnet" "cba_private" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-2b"
 
   tags = {
-    Name = "Terraform-public-subnet02"
+    Name = "ApachePrivateSubnet"
   }
 }
 
-#Creating Route Table
-resource "aws_route_table" "route" {
-  vpc_id = aws_vpc.Terra-vpc.id
+resource "aws_internet_gateway" "cba_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "ApacheIGW"
+  }
+}
+
+resource "aws_route_table" "cba_public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.Terraform-IGW.id
-  }
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id = aws_internet_gateway.Terraform-IGW.id
+    gateway_id = aws_internet_gateway.cba_igw.id
   }
 
   tags = {
-    Name = "Route to internet"
-  }
-}
-
-#Associate Route tables to subnets
-resource "aws_route_table_association" "rt1" {
-  subnet_id      = aws_subnet.terraform_public_subnet01.id
-  route_table_id = aws_route_table.route.id
-}
-
-resource "aws_route_table_association" "rt2" {
-  subnet_id      = aws_subnet.terraform_public_subnet02.id
-  route_table_id = aws_route_table.route.id
-}
-
-#Creating Security Group for ELB
-resource "aws_security_group" "ELB_SG" {
-  name        = "ELB Security Group"
-  description = "ELB Security Group"
-  vpc_id      = aws_vpc.Terra-vpc.id
-
-  # Inbound Rules
-  # HTTP access from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # HTTPS access from anywhere
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    # SSH access from anywhere
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  # Outbound Rules
-  # Internet access to anywhere
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Creating Elastic Load Balancer
-resource "aws_elb" "web_elb" {
-  name = "web-elb"
-  security_groups = [
-    "${aws_security_group.ELB_SG.id}"
-  ]
-  subnets = [
-    "${aws_subnet.terraform_public_subnet01.id}",
-    "${aws_subnet.terraform_public_subnet02.id}"
-  ]
-
-  cross_zone_load_balancing = true
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    target              = "HTTP:80/"
-  }
-
-  listener {
-    lb_port           = 80
-    lb_protocol       = "http"
-    instance_port     = "80"
-    instance_protocol = "http"
+    "Name" = "ApachePublicRT"
   }
 
 }
 
-# Creating launch Template
-resource "aws_launch_configuration" "web" {
-  name_prefix = "web-"
-
-  image_id      = data.aws_ssm_parameter.instance_ami.value
-  instance_type = var.instance_type
-  key_name      = var.keyname
-
-  security_groups             = ["${aws_security_group.ELB_SG.id}"]
-  associate_public_ip_address = true
-  user_data                   = file("data.sh")
-
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_route_table_association" "cba_subnet_rt_public" {
+  subnet_id      = aws_subnet.cba_public.id
+  route_table_id = aws_route_table.cba_public_rt.id
 }
 
-
-#Creating Auto Scaling Group
-resource "aws_autoscaling_group" "web" {
-  name = "${aws_launch_configuration.web.name}-asg"
-
-  min_size         = 3
-  desired_capacity = 3
-  max_size         = 3
-
-  health_check_type = "ELB"
-  load_balancers = [
-    "${aws_elb.web_elb.id}"
-  ]
-
-  launch_configuration = aws_launch_configuration.web.name
-
-  enabled_metrics = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupTotalInstances"
-  ]
-
-  metrics_granularity = "1Minute"
-
-  vpc_zone_identifier = [
-    "${aws_subnet.terraform_public_subnet01.id}",
-    "${aws_subnet.terraform_public_subnet02.id}"
-  ]
-
-  # Required to redeploy without an outage.
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag {
-    key                 = "Name"
-    value               = "web"
-    propagate_at_launch = true
-  }
-
-}
-
-
-#Creating Auto scaling group policy for scaling-up
-resource "aws_autoscaling_policy" "web_policy_up" {
-  name                   = "web_policy_up"
-  scaling_adjustment     = 1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-}
-
-
-# Creating the Cloudwatch metric alarm for scaling-up
-resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
-  alarm_name          = "web_cpu_alarm_up"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "70"
-
-  dimensions = {
-    AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
-  }
-
-  alarm_description = "This metric monitor EC2 instance CPU utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.web_policy_up.arn}"]
-}
-
-
-#Creating the Auto scaling policy for scaling-down
-resource "aws_autoscaling_policy" "web_policy_down" {
-  name                   = "web_policy_down"
-  scaling_adjustment     = -1
-  adjustment_type        = "ChangeInCapacity"
-  cooldown               = 300
-  autoscaling_group_name = aws_autoscaling_group.web.name
-}
-
-
-#Creating the Cloudwatch metric alarm for scaling down
-resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_down" {
-  alarm_name          = "web_cpu_alarm_down"
-  comparison_operator = "LessThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = "120"
-  statistic           = "Average"
-  threshold           = "30"
-
-  dimensions = {
-    AutoScalingGroupName = "${aws_autoscaling_group.web.name}"
-  }
-
-  alarm_description = "This metric monitor EC2 instance CPU utilization"
-  alarm_actions     = ["${aws_autoscaling_policy.web_policy_down.arn}"]
-}
-
-
+data "aws_region" "current" {}
--- variable.tf
+++ variable.tf
@@ -1,26 +1,11 @@
-# Defining Key Name for connection
-variable "keyname" {
-  default     = "cba-keypair"
-  description = "cba-keypair"
+variable "region" {
+  default = "eu-west-2"
 }
 
-# Defining CIDR Block for VPC
-variable "vpc_cidr" {
-  default = "10.1.0.0/16"
-}
-
-# Defining CIDR Block for 1st Public Subnet
-variable "subnet1_cidr" {
-  default = "10.1.1.0/24"
-}
-
-# Defining CIDR Block for 2nd Public Subnet
-variable "subnet2_cidr" {
-  default = "10.1.2.0/24"
-}
-
-# Defining instance type
 variable "instance_type" {
   default = "t2.micro"
 }
 
+variable "key_name" {
+  default = "cba-keypair"
+}
